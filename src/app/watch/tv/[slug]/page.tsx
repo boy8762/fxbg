91467cb752fe33a185031b964db2e4f3a9d9f16e
boy8762 <boya93266@gmail.com>
2025-04-21 12:25:37 +0530

    Update page.tsx

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -167,7 +167,8 @@ const WatchPage: React.FC = () => {
 
   // List of embed API providers with custom names
   const embedSources = [
-    { name: "Netprime Server 1 { Multi }", url: `https://vidzee.wtf/tv/multi.php?id=${id}/${selectedSeason}/${episode}` },
+    { name: "Netprime Server 1 { Multi }", url: `https://Letsembed.cc/embed/tv/?id=${id}/${selectedSeason}/${episode}` },
+    { name: "Netprime Server 7", url: `https://vidzee.wtf/tv/multi.php?id=${id}&season=${selectedSeason}&episode=${episode}` },
     { name: "Netprime Server 1 {Anime} { Multi }", url: `https://api.flixindia.site/anime/${id}/${selectedSeason}/${episode}` },
     { name: "Netprime Server 2 {Og}", url: `https://vidzee.wtf/tv/?id=${id}/${selectedSeason}/${episode}` },
     { name: "Netprime Server 3", url: `https://vidlink.pro/tv/${id}/${selectedSeason}/${episode}` },
